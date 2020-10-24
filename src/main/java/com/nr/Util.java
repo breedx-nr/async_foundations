6204c5322b22190a6d2528c361c02beec274a270
jplumb <jplumb@newrelic.com>
2020-10-24 12:45:51 -0700

    Create skel

---
+++ src/main/java/com/nr/Util.java
@@ -0,0 +1,7 @@
+package com.nr;
+
+public class Util {
+
+
+
+}
---
+++ src/main/java/com/nr/async1/FullySynchronous.java
@@ -0,0 +1,10 @@
+package com.nr.async1;
+
+/**
+ * What does it take to do 3 requests?
+ */
+public class FullySynchronous {
+    public static void main(String[] args) {
+
+    }
+}
